8e666939d44636b731d9acf5b890dab206814a6e
vitek <vitek0585@mail.ru>
2016-02-01 18:58:55 +0200

    base skeleton of onion architecture

---
+++ FashionStore/Scripts/application/app-module/app.js
@@ -0,0 +1,21 @@
+﻿Array.prototype.remove = function (item, field) {
+
+    var array = this;
+    for (var i = 0; i < array.length; i++) {
+
+        if (array[i][field] == item) {
+            array.splice(i, 1);
+            break;
+        }
+    }
+
+};
+(function() {
+    'use strict';
+
+    var global = angular.module("app", []);
+
+    global.injectRequires = function(arr) {
+        Array.prototype.push.apply(this.requires, arr);
+    }
+})();
